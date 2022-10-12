b49734f13726f234419524ef1a01002b6ae540ec
Jamal Muslimov <mechanoid0415@gmail.com>
2022-10-12 22:05:47 +0300

    feat: solve delete-digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const length = Math.ceil(Math.log10(n)) // Длина числа: 1056 => 4
+
+  let max = 0 // Наибольшее число без индекса: 1056 => 156
+
+  function removeDigitByIndex(number, index) {
+    const
+      dividedA = number / 10 ** (length - index), // Запятая до индекса: (index = 2, num = 1056) => 10,56
+      dividedB = number / 10 ** (length - index - 1), // Запятая после индекса: (index = 2, num = 1056) => 105,6
+      beforeIndex = Math.trunc(dividedA), // Цифры до индекса: (index = 2, num = 1056) => 105
+      afterIndex = Math.round((dividedB - Math.trunc(dividedB)) * 10 ** (length - index - 1)), // Цифры после индекса: (index = 2, num = 1056) => 6
+      value = beforeIndex * 10 ** (length - index - 1) + afterIndex // Число без индекса: (index = 2, num = 1056) => 106
+
+    return value
+  }
+
+  for (let i = 0; i < length; i++) {
+    max = Math.max(removeDigitByIndex(n, i), max)
+  }
+
+  return max
 }
 
 module.exports = {
