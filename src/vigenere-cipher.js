98161606249b704c55af8da93513ac62c265ca62
Jamal Muslimov <mechanoid0415@gmail.com>
2022-10-15 22:14:32 +0300

    feat: partially solved vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(direction = true) {
+    this.direction = direction
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  #alphabetLength = 26
+
+  #dir = (arr = []) => this.direction ? arr : arr.reverse()
+  #isLatin = (char) => /[A-Z]/.test(char)
+  #getAlphabetPosition = (char) => char.toLowerCase().charCodeAt() - 96
+  #wrap = (n, max = this.#alphabetLength) => {
+    n %= max
+    return n < 0 ? max + n : n
+  }
+
+  #crypt(string, key, func) {
+    if (!(arguments[0] && arguments[1])) throw new Error('Incorrect arguments!')
+    string = string.toUpperCase()
+    key = key.toUpperCase()
+    let codes = []
+    for (let i = 0, keyOffset = 0; i < string.length; i++) {
+      let char = string[i];
+      if (this.#isLatin(char)) {
+        char = this.#wrap(func(this.#getAlphabetPosition(char), this.#getAlphabetPosition(key[this.#wrap(keyOffset, key.length)]))) + 96
+        keyOffset++
+      }
+      codes.push(char)
+
+    }
+    return this.#dir(codes.map(char => Number.isInteger(char) ? String.fromCharCode(char) : char)).join("").toUpperCase()//, keys.join(" ")
+  }
+
+  encrypt(string = "", key = "") {
+    return this.#crypt(string, key, (a, b) => a + b - 1)
+  }
+  decrypt(string, key) {
+    return this.#crypt(string, key, (a, b) => a - b + 1)
   }
 }
+
+const enc = "attack at dawn!"
+const dec = "AEIHQX SX DLLU!"
+const directMachine = new VigenereCipheringMachine();
+const reverseMachine = new VigenereCipheringMachine(false);
+
+console.log(directMachine.decrypt(dec, "alphonse"))
+console.log(directMachine.encrypt(enc, "alphonse"))
+//console.log(directMachine.encrypt())
+// console.log(directMachine.wrap(-7))
+
+
 
 module.exports = {
   VigenereCipheringMachine
