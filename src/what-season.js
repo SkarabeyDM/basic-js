291b8c61f574039c033ce27636cb2dbc4108fd5b
Jamal Muslimov <mechanoid0415@gmail.com>
2022-10-16 12:46:22 +0300

    feat: solve what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,74 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!arguments.length) return 'Unable to determine the time of year!'
+  if (date.constructor.name !== "Date") throw new Error("Invalid date!")
+  try {
+    date.valueOf()
+  } catch (e) {
+    throw new Error("Invalid date!")
+  }
+
+  const seasons = {
+    0: "winter",
+    1: "winter",
+    2: "spring",
+    3: "spring",
+    4: "spring",
+    5: "summer",
+    6: "summer",
+    7: "summer",
+    8: "autumn",
+    9: "autumn",
+    10: "autumn",
+    11: "winter"
+  }
+
+  return seasons[date.getMonth()]
 }
+
+// const date = new Date(1900, 1, 22, 23, 45, 11, 500)
+// const fakeDate = {
+//   toString() {
+//     return Date.prototype.toString.call(new Date());
+//   },
+//   [Symbol.toStringTag]: 'Date'
+// };
+// Object.setPrototypeOf(fakeDate, Object.getPrototypeOf(new Date()));
+// const deeperFakeDate = {
+//   toString() {
+//     return Date.prototype.toString.call(new Date());
+//   },
+//   getMonth() {
+//     return Date.prototype.getMonth.call(new Date());
+//   },
+//   getFullYear() {
+//     return Date.prototype.getFullYear.call(new Date(1994, 1, 2, 3, 4, 5));
+//   },
+//   getDate() {
+//     return Date.prototype.getDate.call(new Date(2020, 0, 3, 4, 5, 6));
+//   },
+//   getHours() {
+//     return Date.prototype.getHours.call(new Date(1978, 2, 4, 5, 6, 7));
+//   },
+//   getMinutes() {
+//     return Date.prototype.getMinutes.call(new Date(202, 3, 5, 6, 7, 8));
+//   },
+//   getSeconds() {
+//     return Date.prototype.getSeconds.call(new Date(1, 4, 6, 7, 8, 9));
+//   },
+//   getMilliseconds() {
+//     return Date.prototype.getMilliseconds.call(new Date(2019, 7, 8, 9, 10, 11));
+//   },
+//   getDay() {
+//     return Date.prototype.getDay.call(new Date(1812, 8, 9, 10, 11, 12));
+//   },
+//   [Symbol.toStringTag]: 'Date'
+// };
+
+// console.log(getSeason(date))
+// console.log(getSeason(fakeDate))
 
 module.exports = {
   getSeason
